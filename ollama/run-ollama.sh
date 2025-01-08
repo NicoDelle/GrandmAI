2dfce4683bda3ba312fe1ed46a7bf5dc1d3d4b76
Nicolo Dellepiane <nicolo.dellepiane@mail.polimi.com>
2025-01-08 23:16:51 +0100

    Changed model to qwen2.5:7b to allow faster response time

--- ollama/run-ollama.sh
+++ ollama/run-ollama.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 ollama serve &
 sleep 5
-ollama pull qwen2.5:14b
+#ollama pull qwen2.5:14b
+ollama create grandmai -f /Modelfile
 ollama pull nomic-embed-text
 
 if [ $? -eq 0 ]; then
